cda382d264077c69285b10f6c4dbcc9aac89372e
Ayronhayd <128861928+Ayronhayd@users.noreply.github.com>
2023-12-19 19:25:46 +0300

    Меню открывается закрывается

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+const opened = document.querySelector('.page-header__button--opened');
+const site = document.querySelector('.page-header__nav');
+const togler = document.querySelector('.page-header__togler');
+
+opened.onclick = function () {
+  opened.classList.toggle('page-header__button--opened');
+  opened.classList.toggle('page-header__button--closed');
+  if (togler.classList.contains('page-header__button--opened')) {
+    site.classList.add('page-header__nav-closed');
+  } else {
+    site.classList.remove('page-header__nav-closed');
+  }
+};
+

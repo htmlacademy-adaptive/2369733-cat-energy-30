c97f6c1f7233e25274a8a72450e967b336605f71
Ayronhayd <128861928+Ayronhayd@users.noreply.github.com>
2023-12-21 21:05:43 +0300

    nav

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,11 +1,11 @@
-const opened = document.querySelector('.page-header__button--opened');
+const opened = document.querySelector('.page-header__togler--opened');
 const site = document.querySelector('.page-header__nav');
 const togler = document.querySelector('.page-header__togler');
 
 opened.onclick = function () {
-  opened.classList.toggle('page-header__button--opened');
-  opened.classList.toggle('page-header__button--closed');
-  if (togler.classList.contains('page-header__button--opened')) {
+  opened.classList.toggle('page-header__togler--opened');
+  opened.classList.toggle('page-header__togler--closed');
+  if (togler.classList.contains('page-header__togler--opened')) {
     site.classList.add('page-header__nav-closed');
   } else {
     site.classList.remove('page-header__nav-closed');

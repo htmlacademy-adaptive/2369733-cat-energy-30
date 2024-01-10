f0ce6816fef66cd44d2a635950c8828baa2d4449
Ayronhayd <128861928+Ayronhayd@users.noreply.github.com>
2024-01-10 19:00:35 +0300

    pixel  - js не готов

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,6 +3,7 @@ window.addEventListener('load', () => {
   const navMenu = document.querySelector('.js-nav-menu');
 
   navMenu.classList.remove('is-active');
+  button.classList.remove('no--js');
 
   button.addEventListener('click', (e) => {
     e.preventDefault();

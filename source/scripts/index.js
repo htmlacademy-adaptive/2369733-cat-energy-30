00769d83ece65e2741e17f883190f0e7c1d8af05
Ayronhayd <128861928+Ayronhayd@users.noreply.github.com>
2024-01-13 11:55:50 +0300

    save

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,9 +1,12 @@
 window.addEventListener('load', () => {
   const button = document.querySelector('.js-burger');
   const navMenu = document.querySelector('.js-nav-menu');
+  const headerWr = document.querySelector('.header__wrraper');
 
   navMenu.classList.remove('is-active');
   button.classList.remove('no--js');
+  headerWr.classList.remove('header--no-js');
+  navMenu.classList.remove('site-list--no-js');
 
   button.addEventListener('click', (e) => {
     e.preventDefault();
